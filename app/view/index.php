f7e4346609a8ce0e050ebc1e824bd6be49b251de
ACH-S-M <MuhamadihsanM101@gmail.com>
2025-04-03 14:23:58 +0700

    udah bisa keubah form nya

--- app/view/index.php
+++ app/view/index.php
@@ -13,17 +13,39 @@ include '../koneksi/koneksi.php';
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 </head>
 <body  class="bg-blue-200">
-        <div class="loginpage w-3/4 h-auto mx-auto my-24 pb-7 bg-white rounded-md  " >
-           <div class="menu flex gap-7 pb-4 text-2xl cursor-pointer pl-2 pt-2">
-            <h1 class="bg-blue-500 text-white pl-2 pr-2 ">Login</h1>
-            <h1>Daftar </h1>
-           </div>
-           <h1>Login Page </h1>
-                <form action="login" method="post" class="h-full flex flex-col justify-center items-center m-6 gap-4" >
-                    <input type="text" placeholder="username" class="border-2 p-2 w-1/2">
-                    <input type="password" placeholder="Password" class="border-2 p-2 w-1/2">
-                    <button type="submit" class="bg-purple-400 text-white p-3 w-1/2 rounded-md">Login</button>
-                </form>
+<div class=" w-3/4 h-auto mx-auto my-24 pb-7 bg-white rounded-md  " >
+        <div class="menu flex gap-7 pb-4 text-2xl cursor-pointer pl-2 pt-2">
+         <h1 onclick="ubahHalaman('../asset/html/loginpage.html')">Login</h1>
+         <h1 onclick="ubahHalaman('../asset/html/signuppage.html')">Daftar </h1>
         </div>
+             <form action="login" method="post" class="loginpage h-full flex flex-col justify-center items-center m-6 gap-4" >
+                 <input type="text" placeholder="username" class="border-2 p-2 w-1/2">
+                 <input type="password" placeholder="Password" class="border-2 p-2 w-1/2">
+                 <button type="submit" class="bg-purple-400 text-white p-3 w-1/2 rounded-md">Login</button>
+             </form>
+     </div>
+    <script>
+            function ubahHalaman(el){
+                if (el === '../asset/html/loginpage.html'){
+                    alert ('login ditekan ')
+                }else if(el === '../asset/html/signuppage.html') {
+                    alert ('daftar ditekan ')
+                }
+                fetch(el)
+                .then(response => {
+                        if (!response.ok) {
+                            throw new Error("HTTP error! Status: " + response.status);
+                        }
+                        return response.text();
+                    })
+                    .then(html => {
+                            console.log("HTML Loaded:", html);
+                            document.querySelector('.loginpage').innerHTML = html;
+                        })
+                        .catch(error => {
+                            console.error("Gagal memuat halaman:", error);
+                        });
+            }
+    </script>
 </body>
 </html>
