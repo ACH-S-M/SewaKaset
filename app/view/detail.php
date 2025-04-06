dbfa709b4adab2b68e177dfc9b33ac6fd62638ba
ACH-S-M <MuhamadihsanM101@gmail.com>
2025-04-06 17:03:25 +0700

    layout udah hampir jadi

--- app/view/detail.php
+++ app/view/detail.php
@@ -25,7 +25,17 @@ include 'navbarbg.php';
              <img src="data:image/jpeg;base64,<?= base64_encode($data['foto_produk']) ?>" alt="foto"  class="w-[400px] h-[300px] object-contain rounded-md">
                 <div class="title-content">
                     <h1 class="text-3xl font-bold" ><?php echo $data['namakaset']?> </h1>
-                    <h1 class="text-xl " ><?php echo 'IDR. '.$data['harga'].',00'?> </h1>
+                    <h1 class="text-xl " ><?php echo 'Rp. '.$data['harga'].',00'?> </h1>
+                    <p class="mt-5">Deskripsi : </p>
+                        <p class="text-m">
+                                <?php echo $data['deskripsi']?>
+                         </p>
+                    <div class="action-btn p-9">
+                        <form action="" class="flex gap-12">
+                            <button class="bg-blue-800 w-auto rounded-md h-11 text-white px-10 py-1">Beli <?php echo $data['harga']?></button>
+                            <button class="bg-green-500  h-11 rounded-md text-white px-10 py-1">Sewa </button>
+                        </form>
+                    </div>
                 </div>
        
         </section>
